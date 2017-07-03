650ec060410b6d4357bffd9b8eba7cd07f6b168e
vladimir <yovchenko.w@gmail.com>
2017-07-03 21:12:09 +0300

    initial

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
+	 var opacity = 0.55;
+     var pulse = setInterval( function() {
+		   if (opacity < 0.95) {
+	       $('.entry-title').css('color','rgba(255%,255%,255%,'+ opacity + ')');
+		   opacity += 0.05;
+		   }
+		   else { 
+           clearInterval(pulse);
+		   }
+    }, 100);
+ 
 	setTimeout(function(){
 		$('body').addClass('loaded');
 		$('.entry-title').css('display','none');
 	}, 2500);
-
 });
