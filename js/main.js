142f826af76ba69602a4378e1ef9144d84e03da2
vladimir <yovchenko.w@gmail.com>
2017-07-12 00:10:02 +0300

    svg goes to footer

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-$(document).ready(function() {
-	 var opacity = 0.55;
-     var pulse = setInterval( function() {
-		   if (opacity < 0.95) {
-	       $('.entry-title').css('color','rgba(255%,255%,255%,'+ opacity + ')');
-		   opacity += 0.05;
-		   }
-		   else { 
-           clearInterval(pulse);
-		   }
-    }, 100);
-	setTimeout(function(){
+/*preloader*/
+$(document).ready(function () {
+	var opacity = 0.55;
+	var pulse = setInterval(function () {
+		if (opacity < 0.95) {
+			$('.entry-title').css('color', 'rgba(255%,255%,255%,' + opacity + ')');
+			opacity += 0.05;
+		}
+		else {
+			clearInterval(pulse);
+		}
+	}, 100);
+
+	setTimeout(function () {
 		$('body').addClass('loaded');
-		$('.entry-title').css('display','none');
+		$('.entry-title').css('display', 'none');
 	}, 2500);
+});
+
+/* hamburger menu */
+$(document).ready(function () {
+	$('.menuToggle').on('click', function () {
+		$(this).toggleClass('active');
+	});
+	$('.menuToggle').hover(function () {
+		$('.menuToggle span').toggleClass('spanWithShadow');
+		$('.menuToggle').toggleClass('menuTransform');
+	});
 });
