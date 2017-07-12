c1e9bb9018f85b7294d240fc4f35b5b442f3409b
vladimir <yovchenko.w@gmail.com>
2017-07-13 00:41:29 +0300

    added header text

--- js/main.js
+++ js/main.js
@@ -15,51 +15,78 @@ $(document).ready(function () {
 		$('body').addClass('loaded');
 		$('.entry-title').css('display', 'none');
 	}, 2500);
-});
 
-/* hamburger menu */
-$(document).ready(function () {
-	$('.menuToggle').on('click', function () {
-		$(this).toggleClass('active');
-	  headerText();
-	});
+	/* hamburger menu */
 	$('.menuToggle').hover(function () {
 		$('.menuToggle span').toggleClass('spanWithShadow');
 		$(this).toggleClass('menuTransform');
+		$(this).click(function () {
+			var $this = $(this);
+			if ($this.hasClass('active')) {
+				$this.removeClass('active');
+				flag = false;
+				headerText();
+			}
+			else {
+				$this.addClass('active');
+				flag = true;
+				headerText();
+			}
+		});
 	});
 });
-
 /*header text*/
-  var headerText = function() {
-  var s;
-  textLetters = {
-    settings: {
-      lettersAbout: $('.js-about'),
-			lettersWork: $('.js-work'),
-			lettersContact: $('.js-contact'),
-    },
-    init: function() {
-      s = this.settings;
-      this.bindEvents();
-    },
-    bindEvents: function(){
-	  	var about =	$('.js-about').text('About');
-			var work =	$('.js-work').text('Work');	
-			var contact =	$('.js-contact').text('Contact');	
-      s.lettersAbout.html(function (i, el) {
-        var text = $.trim(el).split("");
-        return '<span>' + text.join('</span><span>') + '</span>';
-      });
- 			s.lettersWork.html(function (i, el) {
-        var text = $.trim(el).split("");
-        return '<span>' + text.join('</span><span>') + '</span>';
-      });
-			 s.lettersContact.html(function (i, el) {
-        var text = $.trim(el).split("");
-        return '<span>' + text.join('</span><span>') + '</span>';
-      });
-    },
-  };
-  textLetters.init();
+var flag = true;
+var headerText = function () {
+	var $about, $work, $contact;
+	var s,
+		textLetters = {
+			settings: {
+				lettersAbout: $('.js-about'),
+				lettersWork: $('.js-work'),
+				lettersContact: $('.js-contact'),
+			},
+			init: function () {
+				s = this.settings;
+				this.bindEvents();
+			},
+			bindEvents: function () {
+				if (flag == true) {
+					$about = $('.js-about').text('About');
+					$work = $('.js-work').text('Work');
+					$contact = $('.js-contact').text('Contact');
+					s.lettersAbout.html(function (i, el) {
+						var text = $.trim(el).split("");
+						return '<span>' + text.join('</span><span>') + '</span>';
+					});
+					s.lettersWork.html(function (i, el) {
+						text = $.trim(el).split("");
+						return '<span>' + text.join('</span><span>') + '</span>';
+					});
+					s.lettersContact.html(function (i, el) {
+						text = $.trim(el).split("");
+						return '<span>' + text.join('</span><span>') + '</span>';
+					});
+				}
+				else {
+					$about = $('.js-about').text('');
+					$work = $('.js-work').text('');
+					$contact = $('.js-contact').text('');
+					s.lettersAbout.html(function (i, el) {
+						text = $.trim(el).split("");
+						return text;
+					});
+					s.lettersWork.html(function (i, el) {
+						text = $.trim(el).split("");
+						return text;
+					});
+					s.lettersContact.html(function (i, el) {
+						text = $.trim(el).split("");
+						return text;
+					});
+				}
+			},
+		};
+	textLetters.init();
 };
 
