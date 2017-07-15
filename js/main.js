6fdc7b0c2db4f207867d28d7d74bf5e37dc7cd3f
vladimir <yovchenko.w@gmail.com>
2017-07-15 21:36:53 +0300

    changed hamburger menu size

--- js/main.js
+++ js/main.js
@@ -15,67 +15,68 @@ $(document).ready(function () {
 		$('body').addClass('loaded');
 		$('.entry-title,#loader').css('display', 'none');
 	}, 2500);
-	
-/* hamburger menu */
+
+	/* hamburger menu */
 	$('.menuToggle').hover(function () {
 		$('.menuToggle span').toggleClass('spanWithShadow');
 		$(this).toggleClass('menuTransform');
 	});
-});
 
 	$('.menuToggle').click(function () {
-			var $this = $(this);
-			if ($this.hasClass('active')) {
-				$this.removeClass('active');
-				$('.menu__about,.menu__work,.menu__contact').addClass('fadeOut');
-				headerText();
-				setTimeout(function () {
-					$('.menu__init').css('display','none');
-					$('.fadeOut').css('display','none');
-	             }, 1800);
-			}
-			else {
-				$this.addClass('active');
-				$('.menu__init').css('display','none');
-				$('.menu__about,.menu__work,.menu__contact').removeClass('fadeOut').css('display','flex');
-				headerText();
-			}
-		});
+		var $setT, $resetT;
+		var $this = $(this);
+		var $menuClasses = $('.menu__about,.menu__work,.menu__contact');
+		var $init = $('#menu__init');
+		if ($this.hasClass('active')) {
+			$this.removeClass('active');
+			$menuClasses.addClass('fadeOut');
+			var $set = setTimeout(function () {
+				$('.fadeOut').css('display', 'none');
+			}, 1600);
+		}
+		else {
+			$this.addClass('active');
+			$menuClasses.removeClass('fadeOut').css('display', 'flex');
+			headerText();
+		}
+	});
 
-/*header text*/
-var headerText = function () {
-	var $about, $work, $contact;
-	var s,
-		textLetters = {
-			settings: {
-				lettersAbout: $('.js-about'),
-				lettersWork: $('.js-work'),
-				lettersContact: $('.js-contact'),
-			},
-			init: function () {
-				s = this.settings;
-				this.bindEvents();
-			},
-			bindEvents: function () {
-				$about = $('.js-about').text('About');
-				$work = $('.js-work').text('Work');
-				$contact = $('.js-contact').text('Contact');
-				{
-					s.lettersAbout.html(function (i, el) {
-						var text = $.trim(el).split("");
-						return '<span>' + text.join('</span><span>') + '</span>';
-					});
-					s.lettersWork.html(function (i, el) {
-						text = $.trim(el).split("");
-						return '<span>' + text.join('</span><span>') + '</span>';
-					});
-					s.lettersContact.html(function (i, el) {
-						text = $.trim(el).split("");
-						return '<span>' + text.join('</span><span>') + '</span>';
-					});
-				}
-			},
-		};
-	textLetters.init();
-};
+	/*header text*/
+	var headerText = function () {
+		var $about, $work, $contact;
+		var s,
+			textLetters = {
+				settings: {
+					lettersAbout: $('.js-about'),
+					lettersWork: $('.js-work'),
+					lettersContact: $('.js-contact'),
+				},
+				init: function () {
+					s = this.settings;
+					this.bindEvents();
+				},
+				bindEvents: function () {
+					$about = $('.js-about').text('About');
+					$work = $('.js-work').text('Work');
+					$contact = $('.js-contact').text('Contact');
+					{
+						s.lettersAbout.html(function (i, el) {
+							var text = $.trim(el).split("");
+							return '<span>' + text.join('</span><span>') + '</span>';
+						});
+						s.lettersWork.html(function (i, el) {
+							text = $.trim(el).split("");
+							return '<span>' + text.join('</span><span>') + '</span>';
+						});
+						s.lettersContact.html(function (i, el) {
+							text = $.trim(el).split("");
+							return '<span>' + text.join('</span><span>') + '</span>';
+						});
+					}
+				},
+			};
+		textLetters.init();
+	};
+});
+
 
