1540c931e6ab59036876e87f03100e311b1f5bf1
vladimir <yovchenko.w@gmail.com>
2017-07-16 19:25:21 +0300

    initial

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 
 	setTimeout(function () {
 		$('body').addClass('loaded');
-		$('.entry-title,#loader').stop().css('display','none');
+		$('.entry-title,#loader,#loader:before,#loader:after').stop().css('display','none');
 	}, 2500);
 
 	/* hamburger menu */
