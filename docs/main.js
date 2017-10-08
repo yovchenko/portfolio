547fcf49416c9bd0e48c3d19a25bfb0496bf86d3
yovchenko <yovchenko.w@gmail.com>
2017-10-08 22:59:34 +0300

    created new docs

--- docs/main.js
+++ docs/main.js
@@ -15,7 +15,7 @@
 /******/ 			l: false,
 /******/ 			exports: {}
 /******/ 		};
-/******/
+/*****/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/
