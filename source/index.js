589216471789c968bf50ae65b0344f640b02cd3b
yovchenko <yovchenko.w@gmail.com>
2017-10-08 17:20:31 +0300

    changed root folder

--- source/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './scss/main.scss';
-import styles from 'normalize-scss';
-import _ from 'jquery';
-import './js/preloader';
-import './js/canvasBackground';
-import './js/main';
-import './js/canvasSphere.js';
-import './js/contactForm';
-
