51b3a697ee700cd979063d3cb28d5da3a4026c28
yovchenko <yovchenko.w@gmail.com>
2017-10-08 18:43:47 +0300

    rebase branch

--- webpack.config.js
+++ webpack.config.js
@@ -14,14 +14,13 @@ module.exports = {
         inline: true,
         progress: true,
         contentBase: PATHS.docs,
-        port: 8081,
+        port: 8082,
       },
     entry: PATHS.source + '/index.js',
     output: {
         path: PATHS.docs,
         publicPath: '',
         filename: "[name].js",
-      
     },
     module: {
         rules: [   
