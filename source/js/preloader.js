a0021ae83260ac6b120dba2a6c1b3947d6127641
yovchenko <yovchenko.w@gmail.com>
2017-10-10 19:34:53 +0300

    changed footer attribute

--- source/js/preloader.js
+++ source/js/preloader.js
@@ -1,8 +1,9 @@
 import Amplitude from "amplitude";
-var opacity = 0.55;
-var increment = 0.05;
-var pulse = setInterval(function () {
-    var el =  document.getElementsByClassName('entry-title')[0];
+"use strict";
+let opacity = 0.55;
+let increment = 0.05;
+let pulse = setInterval(function () {
+    const el =  document.getElementsByClassName('entry-title')[0];
     opacity += increment;
     if (!document.body.classList.contains('loaded') && (!(opacity.toFixed(2) === '0.95' || opacity.toFixed(2) === '0.55')) ) {
         el.style.color = 'rgba(255%,255%,255%,' + opacity + ')';
@@ -17,9 +18,9 @@ var pulse = setInterval(function () {
 
 /*I'm giving you some extra time to enjoy my preloader*/
 window.onload = setTimeout(function () {
-    $('#body').addClass('loaded');
-    $('#footer').css('display', 'block');
-    $('.entry-title,#loader,#loader:before,#loader:after').stop().css('display', 'none');
+    document.body.className = " loaded";
+    document.getElementById('footer').style.display = 'grid';
+    $('#loader').add('.entry-title').add('#loader:before').add('#loader:after').stop().css('display', 'none');
     Amplitude.init({
         "songs":
         [
