77911abf3bc9ed82b1b3a1d9b1a9ae91013773c3
yovchenko <yovchenko.w@gmail.com>
2017-10-24 19:04:22 +0300

    changed volume function

--- docs/main.js
+++ docs/main.js
@@ -37885,7 +37885,10 @@ return $;
 
 /***/ }),
 /* 135 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
 
 jQuery(document).ready(function(){
     // This button will increment the value
@@ -37893,9 +37896,9 @@ jQuery(document).ready(function(){
         // Stop acting like a button
         e.preventDefault();
         // Get the field name
-        fieldName = $(this).attr('data-field');
+       let fieldName = $(this).attr('data-field');
         // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        let currentVal = parseInt($('input[name='+fieldName+']').val());
         // If is not undefined
         if (!isNaN(currentVal) && currentVal < 100) {
             // Increment
@@ -37907,9 +37910,9 @@ jQuery(document).ready(function(){
         // Stop acting like a button
         e.preventDefault();
         // Get the field name
-        fieldName = $(this).attr('data-field');
+       let fieldName = $(this).attr('data-field');
         // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        let currentVal = parseInt($('input[name='+fieldName+']').val());
         // If it isn't undefined or its greater than 0
         if (!isNaN(currentVal) && currentVal > 0) {
             // Decrement one
--- source/js/volumeButtons.js
+++ source/js/volumeButtons.js
@@ -1,12 +1,14 @@
+"use strict";
+
 jQuery(document).ready(function(){
     // This button will increment the value
     $('[data-quantity="plus"]').click(function(e){
         // Stop acting like a button
         e.preventDefault();
         // Get the field name
-        fieldName = $(this).attr('data-field');
+       let fieldName = $(this).attr('data-field');
         // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        let currentVal = parseInt($('input[name='+fieldName+']').val());
         // If is not undefined
         if (!isNaN(currentVal) && currentVal < 100) {
             // Increment
@@ -18,9 +20,9 @@ jQuery(document).ready(function(){
         // Stop acting like a button
         e.preventDefault();
         // Get the field name
-        fieldName = $(this).attr('data-field');
+       let fieldName = $(this).attr('data-field');
         // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        let currentVal = parseInt($('input[name='+fieldName+']').val());
         // If it isn't undefined or its greater than 0
         if (!isNaN(currentVal) && currentVal > 0) {
             // Decrement one
