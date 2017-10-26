89c398ae325cf834b0dcf174c257852872ed2b99
yovchenko <yovchenko.w@gmail.com>
2017-10-27 01:24:03 +0300

    changed form font-size

--- docs/main.js
+++ docs/main.js
@@ -36338,7 +36338,27 @@ var _jqueryValidation2 = _interopRequireDefault(_jqueryValidation);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+jQuery.extend(jQuery.validator.messages, {
+    required: "This field is required.",
+    remote: "Please fix this field.",
+    email: "Please enter a valid email",
+    url: "Please enter a valid URL.",
+    date: "Please enter a valid date.",
+    dateISO: "Please enter a valid date (ISO).",
+    number: "Please enter a valid number.",
+    digits: "Please enter only digits.",
+    creditcard: "Please enter a valid credit card number.",
+    equalTo: "Please enter the same value again.",
+    accept: "Please enter a value with a valid extension.",
+    maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+    minlength: jQuery.validator.format("Please enter at least {0} characters."),
+    rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+    range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+    max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+    min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+});
 var $contactForm = $('#contactForm');
+var button = document.getElementById('submit');
 $contactForm.validate();
 $contactForm.submit(function (e) {
     if ($contactForm.valid()) {
@@ -36349,15 +36369,13 @@ $contactForm.submit(function (e) {
             data: $(this).serialize(),
             dataType: "json",
             beforeSend: function beforeSend() {
-                $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+                button.value = 'Sending message…';
             },
             success: function success(data) {
-                $contactForm.find('.alert--loading').hide();
-                $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+                button.value = 'Message sent!';
             },
             error: function error(err) {
-                $contactForm.find('.alert--loading').hide();
-                $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+                button.value = 'Ops, there was an error.';
             }
         });
     }
--- source/js/contactForm.js
+++ source/js/contactForm.js
@@ -1,6 +1,26 @@
 'use strict';
 import _ from 'jquery-validation';
-let $contactForm = $('#contactForm');
+jQuery.extend(jQuery.validator.messages, {
+    required: "This field is required.",
+    remote: "Please fix this field.",
+    email: "Please enter a valid email",
+    url: "Please enter a valid URL.",
+    date: "Please enter a valid date.",
+    dateISO: "Please enter a valid date (ISO).",
+    number: "Please enter a valid number.",
+    digits: "Please enter only digits.",
+    creditcard: "Please enter a valid credit card number.",
+    equalTo: "Please enter the same value again.",
+    accept: "Please enter a value with a valid extension.",
+    maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+    minlength: jQuery.validator.format("Please enter at least {0} characters."),
+    rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+    range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+    max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+    min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+});
+const $contactForm = $('#contactForm');
+const button = document.getElementById('submit');
 $contactForm.validate();
 $contactForm.submit(function(e) {
     if ($contactForm.valid()) {
@@ -10,11 +30,9 @@ $contactForm.submit(function(e) {
             method: "POST",
             data: $(this).serialize(),
             dataType: "json",
-            beforeSend: function() { $contactForm.append('<div class="alert alert--loading">Sending message…</div>');},
-            success: function(data) {$contactForm.find('.alert--loading').hide();
-            $contactForm.append('<div class="alert alert--success">Message sent!</div>');},
-            error: function(err) { $contactForm.find('.alert--loading').hide();
-            $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>'); }
+            beforeSend: function() { button.value = 'Sending message…'},
+            success: function(data) {button.value = 'Message sent!'},
+            error: function(err) { button.value = 'Ops, there was an error.'}
         });
     }
 });
