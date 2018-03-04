c3f02f91b29817613df3444a4b9450ea4b7d4497
yovchenko <yovchenko.w@gmail.com>
2018-03-04 12:41:06 +0200

    added IE polyfill

--- source/js/flipbook.js
+++ source/js/flipbook.js
@@ -179,7 +179,7 @@ function detectIE() {
   var msie = ua.indexOf('MSIE ');
   if (msie > 0) {
     // IE 10 or older => return version number
-    console.log('ie')
+    console.log('ie');
     return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
   }
 
@@ -187,7 +187,7 @@ function detectIE() {
   if (trident > 0) {
     // IE 11 => return version number
     var rv = ua.indexOf('rv:');
-    console.log('trident')
+    console.log('trident');
     return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
   }
 
