04da14a014769017a153f902cff4a7845a554895
yovchenko <yovchenko.w@gmail.com>
2018-04-02 14:28:07 +0300

    added source-map loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,12 @@ module.exports = {
         filename: "[name].js"
     },
     module: {
-        rules: [{
+        rules: [ {
+            test: /\.js$/,
+            use: ["source-map-loader"],
+            enforce: "pre"
+             },
+            {
                 test: /\.js$/,
                 exclude: [/node_modules/],
                 use: [{
